fbc9ea885569d7fdb278da8941752d06e931f13d
Rohan Sadhukhan <rohan.sadhukhan.56@gmail.com>
2024-04-18 12:28:54 +0530

    fixed useScrollProgress hook

--- hooks/useScrollProgress.tsx
+++ hooks/useScrollProgress.tsx
@@ -1,26 +1,29 @@
 'use client'
-import React , {useState, useEffect} from 'react'
+import React, { useState, useEffect } from 'react';
 
-export default function useScrollProgress() {
-    const [completation , setCompletation] = useState(0)
+export default function useScrollProgress(): number {
+  const [completion, setCompletion] = useState<number>(0);
 
-    useEffect(() => {
-      const UpdateScrollCompletion = () =>{
-        const currentProgress = window.scrollY;
-        const scrollHeight = document.body.scrollHeight - window.innerHeight;
+  useEffect(() => {
+    const updateScrollCompletion = () => {
+      const currentProgress: number = window.scrollY;
+      const scrollHeight: number = document.body.scrollHeight - window.innerHeight;
 
-        if(scrollHeight){
-            setCompletation(Number(currentProgress / scrollHeight).toFixed(2) * 100);
-        }
+      if (scrollHeight) {
+        setCompletion(Number((currentProgress / scrollHeight).toFixed(2)) * 100);
       }
-    
-    //   event 
-    window.addEventListener('scroll',UpdateScrollCompletion);
+    };
 
-    // clear event
-    return () => window.removeEventListener('scroll', UpdateScrollCompletion)
-      
-    }, [])
-    
-  return completation
+    // Add scroll event listener
+    window.addEventListener('scroll', updateScrollCompletion);
+
+    // Clean up event listener on unmount
+    return () => {
+      window.removeEventListener('scroll', updateScrollCompletion);
+    };
+  }, []);
+
+  return completion;
+}
+
 }
